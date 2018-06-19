b006dccdfc369392245efd3ad37b449badfad930
Landon Hymas <38771421+hymasl1@users.noreply.github.com>
2018-06-18 18:10:49 -0600

    JSON Data

---
+++ assignments/lesson-9/js/weatherapi.js
@@ -0,0 +1,14 @@
+var weatherRequest = new XMLHttpRequest();
+weatherRequest.open('GET', 'http://api.openweathermap.org/data/2.5/weather?id=4156210&appid=c0fec883e727184c603e8a3cc5d88e5d&units=imperial', true);
+
+weatherRequest.send();
+
+weatherRequest.onload = function () {
+    var weatherInfo = JSON.parse(weatherRequest.responseText);
+    console.log(weatherInfo);
+
+    document.getElementById('currentTemp').innerHTML = weatherInfo.main.temp;
+
+    var iconcode = weatherInfo.weather[0].icon;
+
+} // end of function
